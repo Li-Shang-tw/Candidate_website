ba3c47b5e98370675a4e763fc175b44a175d2099
LiShang <vuvu0130@gmail.com>
2023-11-15 15:43:01 +0800

    activity section

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,14 @@ import TheDonate from "./components/TheDonate.vue";
 import TheFooter from "./components/TheFooter.vue";
 import TheHeader from "./components/TheHeader.vue";
 import ThePersonalIntro from "./components/ThePersonalIntro.vue";
+import TheActivitySection from "./components/TheActivitySection.vue";
 </script>
 
 <template>
   <TheHeader />
   <TheHero />
   <ThePersonalIntro />
+  <TheActivitySection />
   <TheDonate />
   <TheFooter />
 </template>
---
+++ src/components/TheActivitySection.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="wrap">
+    <div class="activity_section container">
+      <div class="heading">
+        <h3 class="heading__subtitle subtite">policy</h3>
+        <h2 class="heading__title">政策議題</h2>
+      </div>
+      <ul class="policy_selection">
+        <li
+          class="policy_selection__btn"
+          @click="
+            () => {
+              ChangePolicy(1);
+            }
+          "
+        >
+          <img
+            src="../../public/img/政策一.png"
+            alt="政策一"
+            :class="{ filter: policy !== 1 }"
+          />
+        </li>
+        <li
+          class="policy_selection__btn"
+          @click="
+            () => {
+              ChangePolicy(2);
+            }
+          "
+        >
+          <img
+            src="../../public/img/政策二.png"
+            alt="政策二"
+            :class="{ filter: policy !== 2 }"
+          />
+        </li>
+        <li
+          class="policy_selection__btn"
+          @click="
+            () => {
+              ChangePolicy(3);
+            }
+          "
+        >
+          <img
+            src="../../public/img/政策三.png"
+            alt="政策三"
+            :class="{ filter: policy !== 3 }"
+          />
+        </li>
+      </ul>
+      <ActivityContent :policy="policy" />
+      <Button type="2" text="了解更多" class="btn" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import ActivityContent from "./ActivityContent.vue";
+import Button from "./Button.vue";
+
+const policy = ref(1);
+function ChangePolicy(num) {
+  policy.value = num;
+}
+</script>
+
+<style scoped>
+.wrap {
+  background-color: black;
+  padding-bottom: 2em;
+}
+.heading {
+  padding: 2em 0;
+}
+.heading__subtitle {
+  margin-bottom: 0.5em;
+  color: black;
+  -webkit-text-stroke: 0.7px var(--main-bg-color);
+}
+.heading__title {
+  color: var(--main-bg-color);
+}
+
+.policy_selection {
+  display: flex;
+  width: 50%;
+  margin: 0 auto;
+}
+.btn {
+  margin-top: 2em;
+}
+.filter {
+  filter: brightness(0.5);
+}
+</style>
